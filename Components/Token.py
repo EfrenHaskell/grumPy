e2580d4e1d6586f9829fc6d061e358fdf2bb28ca
Efren Haskell <efrenhask@gmail.com>
2023-04-15 00:50:58 -0400

    began work on Token.py

--- Components/Token.py
+++ Components/Token.py
@@ -1,8 +1,18 @@
 '''
-grumPy Token class
+grumPy Token
 
 @author Efren Haskell
 efrenhask@brandeis.edu
 efrenhask@gmail.com
 4/13/2023
 '''
+
+class Token:
+
+    def __init__(self,str: val):
+        self._val = val
+        self._type = self.det_type()
+
+    def det_type(self) -> str:
+        self._val
+        
