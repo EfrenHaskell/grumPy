684fd72c7fd7e5b2bc686bc85abaf144e7530246
Efren Haskell <efrenhask@gmail.com>
2023-05-18 16:11:30 -0400

    finished basic tokenize method

--- Components/Parser.py
+++ Components/Parser.py
@@ -7,7 +7,41 @@ efrenhask@gmail.com
 4/13/2023
 '''
 
+from typing import List
+import Token
+from Error_Log import *
+
 class Parser:
 
-    def build_parse_tree():
+    def __init__(self,input_file:str):
+        self.token_breaks:List[str] = []
+        self.special_tokens:List[str] = []
+
+    def tokenize(self,line:str) -> List[Token.Token]:
+        initial = 0
+        length = len(line)
+        tokens:List[Token.Token] = list()
+        token = ""
+        for index in range(length):
+            if line[index] in self.token_breaks or index == length-1:
+                new = Token.Token()
+                if (initial - index) > 0:
+                    new_token = line[initial:index]
+                    if token in self.special_tokens:
+                        new.set_type(token)
+                        if new_token in self.special_tokens:
+                            throw(mod(SYNTAXERROR," a special token must be followed by a non-special token"))
+                            return None
+                    new.set_val(new_token)
+                    token = new_token
+                    tokens.append(new)
+                    initial = index+1
+            token += line[index]
+        return tokens
+
+    def build_parse_tree(self,tokens:list) -> Token.Token:
+        throw(UNIMPLIMENTED)
+
+    def convert_from_tree(self,root:Token.Token):
+        throw(UNIMPLIMENTED)
         
